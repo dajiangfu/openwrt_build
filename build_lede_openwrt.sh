68b186a4d3f17ce0ed1b8e47431cb23f09f1e810
dajiangfu <60643767+dajiangfu@users.noreply.github.com>
2022-04-18 21:32:41 +0800

    Add files via upload

--- build_lede_openwrt.sh
+++ build_lede_openwrt.sh
@@ -12,15 +12,24 @@ red(){
 
 #初次编译
 function build_openwrt(){
-  sudo apt-get update
-  sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3.5 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib linux-libc-dev:i386
+  #安装编译依赖
+  sudo apt update -y
+  sudo apt full-upgrade -y
+  sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
+  bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib \
+  git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev \
+  libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libreadline-dev libssl-dev libtool lrzsz \
+  mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 python3-pip qemu-utils \
+  rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
+  #下载源代码，更新 feeds 并选择配置
   git clone https://github.com/coolsnowwolf/lede
   cd lede
   ./scripts/feeds update -a
   ./scripts/feeds install -a
   make menuconfig
-  make -j8 download V=s
-  make -j1 V=s
+  #下载dl库，编译固件（-j后面是线程数，第一次编译推荐用单线程）
+  make download -j8
+  make V=s -j1
 }
 
 #重新编译
@@ -38,8 +47,8 @@ function rebuild_openwrt(){
   else
     make defconfig
   fi
-  make -j8 download V=s
-  make -j$(($(nproc) + 1)) V=s
+  make download -j8
+  make V=s -j$(nproc)
 }
 
 #修复并编译(make clean仅仅是清除之前编译的可执行文件及配置文件)
@@ -52,8 +61,8 @@ function repair_build_openwrt(){
   rm -rf ./tmp
   rm -rf .config
   make menuconfig
-  make -j8 download V=s
-  make -j$(($(nproc) + 1)) V=s
+  make download -j8
+  make V=s -j$(nproc)
 }
 
 #完全重新编译(make distclean要清除所有生成的文件)
@@ -66,18 +75,24 @@ function comre_build_openwrt(){
   rm -rf ./tmp
   rm -rf .config
   make menuconfig
-  make -j8 download V=s
-  make -j1 V=s
+  make download -j8
+  make V=s -j1
 }
 
 start_menu(){
   clear
-  green " ===================================="
+  green "====================================="
   green " 介绍：一键编译openwrt(lede版)源码   "
   green " 系统：>=ubuntu14.0                  "
   green " 作者：dajiangfu                     "
   green " 网站：www.github.com/dajiangfu      "
-  green " ===================================="
+  green "====================================="
+  blue  "注意"
+  blue  "1、不要用root用户进行编译"
+  blue  "2、国内用户编译前最好准备好梯子"
+  blue  "3、默认登陆IP 192.168.1.1 密码 password"
+  blue  "4、编译完成后输出路径：bin/targets"
+  green "====================================="
   echo
   green " 1. 初次编译"
   green " 2. 重新编译"
